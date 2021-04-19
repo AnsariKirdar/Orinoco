af533e8200033b8aa43b555f9b3de0298ccaa407
AnsariKirdar <33788641+AnsariKirdar@users.noreply.github.com>
2021-04-20 00:37:51 +0530

    A
    A

---
+++ inc/footer.php
@@ -0,0 +1,89 @@
+<section id="footer">
+  <!-- <div class="container p-0">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 px-3 my-3 px-lg-0 px-md-3 px-sm-3 footerQuickLink">
+        <h3><span>Crystal</span> Ocean</h3>
+        <p>
+          enables individuals to overcome any <br> shipping-related challenges at sea <br>
+          with its in-house LNG training programs.
+        </p>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 px-3 my-3 footerQuickLink">
+        <h3>Course</h3>
+        <ul>
+          <li>
+            <a href="course.php?lchs" data-toggle="tooltip" data-placement="top" title="Liquid Cargo Handling Simulator Course">LCHS</a>
+          </li>
+          <li>
+            <a href="course.php?sts" data-toggle="tooltip" data-placement="top" title="LNG Ship to Ship">LNG STS</a>
+          </li>
+          <li>
+            <a href="course.php?fsru" data-toggle="tooltip" data-placement="top" title="LNG Floating Storage Regasification Unit">LNG FSRU</a>
+          </li>
+          <li>
+            <a href="course.php?lmfbo" data-toggle="tooltip" data-placement="top" title="LNG Marine Fuel &amp; Bunkering Operation">LMFBO</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 px-3 my-3 footerQuickLink">
+        <h3>Company</h3>
+        <ul>
+          <li>
+            <a href="index.php">Home</a>
+          </li>
+          <li>
+            <a href="about.php">About</a>
+          </li>
+          <li>
+            <a href="course.php">Course</a>
+          </li>
+          <li>
+            <a href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 px-3 my-3 footerQuickLink">
+        <h3>Address</h3>
+        <ul>
+          <li>
+            <a href="#" class="d-flex align-items-center">
+              <span class="pr-3 h2 "><i class="fad fa-map-marker-alt" aria-hidden="true"></i></span>
+              <span class="flex-fill">
+                501, Sai Samarth Business Park <br>Deonar, Govandi East, <br>Mumbai 400088 India
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="mail:info@crystalocean.in" class="text-blue text-decoration-none">
+              <span class="pr-3 h4  "><i class="fad fa-envelope-open" aria-hidden="true"></i></span>
+              <span class="flex-fill "> Info@crystalocean.in </span>
+            </a>
+          </li>
+          <li>
+            <a href="tel:919167071117" class="d-flex align-items-center">
+              <span class="pr-3 h4 "><i class="fad fa-phone-alt" aria-hidden="true"></i></span>
+              <span class="flex-fill "> +91 91670 71117 </span>
+            </a>
+
+            <a href="tel:+912235574216" class="d-flex align-items-center">
+              <span class="pr-3 h4"><i class="fad fa-phone-rotary" aria-hidden="true"></i></span>
+              <span class="flex-fill "> +91 22 3557 4216 </span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div> -->
+  <div class="container footerElement">
+    <div class="row text-center">
+      <div class="col-lg-4 col-md-4 col-sm-12 small py-0 fw-bold">© <a href="https://crystaloceanmedia.in/" trget="_blank" class="text-decoration-none "> Crystal Ocean Group </a> 2021. All right reserved.</div>
+      <div class="col-lg-4 col-md-4 col-sm-12 small py-0 fw-bold">Developed By <a href="https://crystaloceanmedia.in/" trget="_blank">Crystal Ocean Media</a></div>
+      <div class="col-lg-4 col-md-4 col-sm-12 py-0">
+        <a href="https://www.facebook.com/crystaloceanmediaandevents/" trget="_blank" class=""> <i class="fab fa-facebook-f" aria-hidden="true"></i></a> &nbsp;
+        <a href="https://twitter.com/Crystal_Ocean_" trget="_blank" class=""> <i class="fab fa-twitter" aria-hidden="true"></i></a> &nbsp;
+        <a href="https://www.youtube.com/channel/UC6BcT6lvn_lHMRzFZMX0FGg" trget="_blank" class=""> <i class="fab fa-youtube" aria-hidden="true"></i></a> &nbsp;
+        <a href="https://www.instagram.com/crystaloceanmedia/" trget="_blank" class=""> <i class="fab fa-instagram" aria-hidden="true"></i></a> &nbsp;
+        <a href="https://wa.me/919167071117" trget="_blank" class=""> <i class="fab fa-whatsapp" aria-hidden="true"></i></a> &nbsp;
+      </div>
+    </div>
+  </div>
+</section>
