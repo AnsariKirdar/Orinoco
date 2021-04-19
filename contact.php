666f08764eb353dc53384030b7830f0a89300834
Kirdar <33788641+AnsariKirdar@users.noreply.github.com>
2021-04-20 00:32:43 +0530

    init
    init

---
+++ contact.php
@@ -0,0 +1,179 @@
+<?php include 'inc/config.php'; ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="title" content="Vision Technology is website Designing Development agency in Mumbai ">
+  <meta name="description" content="Vision Technology, the best creative agency in Mumbai For effective marketing and design solutions. Boost business with our digital marketing Plans. ">
+  <meta name="keywords" content="digital marketing, website designing, website development, management erp, erp and software">
+
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <meta name="language" content="English">
+  <meta name="revisit-after" content="3 days">
+  <meta name="copyright" content="visiontechnology.co.in">
+  <meta name="author" content="Vision technology">
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="./">
+  <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
+  <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+  <meta property="og:image" content=".//assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+  <meta property="profile:first_name" content="Kirdar">
+  <meta property="profile:last_name" content="Ansari">
+  <meta property="business:contact_data:country_name" content="India">
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="./">
+  <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
+  <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+  <meta property="twitter:image" content=".//assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+
+  <link rel="manifest" href="visiontechnology.webmanifest">
+  <link rel="shortcut icon" type="image/x-icon" href="./img/logo/favicon.ico">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/style.css" rel="stylesheet">
+
+
+  <title>Crystal Ocean </title>
+</head>
+
+<body>
+  <?php include  'inc/nav.php'; ?>
+
+
+  <div class="container py-4 pb-5">
+    <div class="continer py-4">
+      <h1 class="text-center fw-bold">
+        Contact Us
+      </h1>
+      <div class="row pb-4">
+        <div class="col-lg-3 col-md-2 col-sm-12">
+        </div>
+        <div class="col-lg-6 col-md-8 col-sm-12">
+          <p class="text-center text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, facere assumenda. Libero ipsum ratione culpa voluptatibus est minima.</p>
+        </div>
+        <div class="col-lg-3 col-md-2 col-sm-12">
+        </div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-lg-6 col-md-12">
+          <img src="img/common/362706457-696x464.jpg" class="w-100 rounded-20" alt="">
+        </div>
+        <div class="col-lg-6 col-md-12">
+          <div class="bg-light p-4 rounded-20 h-100">
+            <h2 class="fw-bold">Drop us a Message</h2>
+            <p class="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos ut totam quam excepturi repudiandae temporibus exercitationem ?</p>
+            <form action="#" method="post" class="contactForm">
+              <div class="form-group py-1">
+                <input type="text" name="" id="" class="form-control form-control-lg  rounded-pill px-3" placeholder="Enter Your Name" aria-describedby="helpId">
+              </div>
+              <div class="form-group py-1">
+                <input type="text" name="" id="" class="form-control form-control-lg  rounded-pill px-3" placeholder="Enter Your Email" aria-describedby="helpId">
+              </div>
+              <div class="form-group py-1">
+                <input type="text" name="" id="" class="form-control form-control-lg  rounded-pill px-3" placeholder="Subject" aria-describedby="helpId">
+              </div>
+              <div class="form-group py-1">
+                <textarea name="" class="form-control form-control-lg  rounded-20" id="" placeholder="Enter Your Message" rows="5"></textarea>
+              </div>
+              <div class="form-group py-1 d-grid">
+                <button type="submit" class="btn btn-primary fw-bold btn-lg rounded-pill">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <div class="container-fluid bg-light py-5 ">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-12">
+          <p class="text-center fw-bold text-primary mb-0">GET IN TOUCH</p>
+        </div>
+        <div class="col-12">
+          <h2 class="text-center fw-bold display-4">We're here to Help</h2>
+        </div>
+        <div class="col-lg-3 col-md-2 col-sm-12">
+        </div>
+        <div class="col-lg-6 col-md-8 col-sm-12">
+          <p class="text-center text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, facere assumenda. Libero ipsum ratione culpa voluptatibus est minima.</p>
+        </div>
+        <div class="col-lg-3 col-md-2 col-sm-12">
+        </div>
+
+
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-12 p-4">
+          <div class="bg-white rounded-20 p-3 shadow">
+            <p class="text-center">
+              <span class="d-flex align-items-center justify-content-center bg-light shadow rounded-circle mx-auto" style="width: 60px; height: 60px;">
+                <i class="fad fa-paper-plane text-primary h4"></i>
+              </span>
+            </p>
+            <p class="text-primary fw-bolder text-center h5 mb-3">
+              Our headoffice address
+            </p>
+            <p class="text-center text-secondary">
+              501, Sai Samarth Business Park, Deonar, <br> Govandi East, Mumbai, Maharashtra 400088
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-12 p-4">
+          <div class="bg-white rounded-20 p-3 shadow">
+            <p class="text-center">
+              <span class="d-flex align-items-center justify-content-center bg-light shadow rounded-circle mx-auto" style="width: 60px; height: 60px;">
+                <i class="fad fa-phone-alt text-primary h4"></i>
+              </span>
+            </p>
+            <p class="text-primary fw-bolder text-center h5 mb-3">
+              Call for Help
+            </p>
+            <p class="text-center text-secondary">
+              +91 ----- ----- <br> +91 ----- -----
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-12 p-4">
+          <div class="bg-white rounded-20 p-3 shadow">
+            <p class="text-center">
+              <span class="d-flex align-items-center justify-content-center bg-light shadow rounded-circle mx-auto" style="width: 60px; height: 60px;">
+                <i class="fad fa-envelope-open-text text-primary h4"></i>
+              </span>
+            </p>
+            <p class="text-primary fw-bolder text-center h5 mb-3">
+              Mail us for Information
+            </p>
+            <p class="text-center text-secondary">
+              info@orinocoshipmanagement.com <br> contact@orinocoshipmanagement.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="container ratio ratio-21x9 rounded-20 overflow-hidden my-4">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.3065040300908!2d72.91583801534463!3d19.050256957730596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c67698b5efe3%3A0x49756729c6d9ccf8!2sCrystal%20Ocean%20Group!5e0!3m2!1sen!2sin!4v1618830444181!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+
+
+  <?php include  'inc/footer.php'; ?>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+  <script src="./js/fontawsome.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,271 @@
+<?php include 'inc/config.php'; ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="title" content="Vision Technology is website Designing Development agency in Mumbai ">
+  <meta name="description" content="Vision Technology, the best creative agency in Mumbai For effective marketing and design solutions. Boost business with our digital marketing Plans. ">
+  <meta name="keywords" content="digital marketing, website designing, website development, management erp, erp and software">
+
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <meta name="language" content="English">
+  <meta name="revisit-after" content="3 days">
+  <meta name="copyright" content="visiontechnology.co.in">
+  <meta name="author" content="Vision technology">
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="./">
+  <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
+  <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+  <meta property="og:image" content=".//assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+  <meta property="profile:first_name" content="Kirdar">
+  <meta property="profile:last_name" content="Ansari">
+  <meta property="business:contact_data:country_name" content="India">
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="./">
+  <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
+  <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+  <meta property="twitter:image" content=".//assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+
+  <link rel="manifest" href="visiontechnology.webmanifest">
+  <link rel="shortcut icon" type="image/x-icon" href="./img/logo/favicon.ico">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/style.css" rel="stylesheet">
+
+  <title>Crystal Ocean </title>
+</head>
+
+<body>
+  <?php include  'inc/nav.php'; ?>
+
+  <div class="container py-4">
+
+    <div class="row align-items-center pb-5">
+
+      <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+        <div class="">
+          <p class="fw-bold text-secondary">Orinoco Ship Management </p>
+          <h1 class="fw-bolder text-dark display-3">
+            <b>Our Exprerienced Crew is here to help you</b>
+          </h1>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+        <img src="img/common/air_lubrication_Maran_Andros.jpg" alt="" class="w-100 rounded-20">
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 col-12 py-4">
+        <p class="fw-bold">01</p>
+        <h3 class="fw-blod"><b>Title</b></h3>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 col-12 py-4">
+        <p class="fw-bold">02</p>
+        <h3 class="fw-blod"><b>Title</b></h3>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 col-12 py-4">
+        <p class="fw-bold">03</p>
+        <h3 class="fw-blod"><b>Title</b></h3>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+        <p class="text-secondary h5 mb-0">Orinoco Ship Management</p>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="bg-light pb-5">
+    <div class="container py-4 bg-light">
+      <div class="continer py-4 bg-light">
+        <h2 class="text-center fw-bold">
+          Best in Industry
+        </h2>
+        <p class="w-50 text-center mx-auto text-secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat numquam facilis dolore reprehenderit corrupti ducimus, veniam repellendus maxime maiores sunt fugiat, hic sed repudiandae officiis rerum veritatis corporis inventore perferendis!
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+          <img src="img/common/photo-1604933762161-67313106146c.jpg" alt="" class="w-100 rounded-20">
+        </div>
+
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+          <p class="fw-bold">30+ Year</p>
+          <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo reprehenderit enim ipsa! Et deserunt repellat quaerat asperiores facilis voluptates, quis dolor alias quia dicta amet assumenda eius voluptate nulla!</p>
+          <p class="fw-bold">30+ Year</p>
+          <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo reprehenderit enim ipsa! Et deserunt repellat quaerat asperiores facilis voluptates, quis dolor alias quia dicta amet assumenda eius voluptate nulla!</p>
+          <p class="fw-bold">30+ Year</p>
+          <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo reprehenderit enim ipsa! Et deserunt repellat quaerat asperiores facilis voluptates, quis dolor alias quia dicta amet assumenda eius voluptate nulla!</p>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+  <div class="container">
+    <div class="row py-5">
+      <div class="col-lg-3 col-md-2 col-sm-12 py-5"></div>
+      <div class="col-lg-6 col-md-8 col-sm-12 py-5">
+        <p class="text-center">Get Started</p>
+        <h2 class="text-center fw-bolder"><b>Get In Touch With Us</b></h2>
+        <hr>
+        <form action="#" method="post">
+          <div class="row">
+
+            <div class="col-12 py-3">
+              <div class="form-group">
+                <textarea name="#" id="" class="rounded-20 form-control form-control-lg p-3 px-4" placeholder="Your Message" rows="6"></textarea>
+              </div>
+            </div>
+
+            <div class="col-lg-9 col-md-9 col-sm-8 col-12 py-3">
+              <div class="form-group">
+                <input type="text" name="" id="" class="form-control form-control-lg rounded-pill px-4" placeholder="Your Email" aria-describedby="helpId">
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-8 col-12 py-3">
+              <div class="form-group d-grid gap-2">
+                <button class="btn  btn-primary btn-lg rounded-pill">
+                  Send
+                </button>
+              </div>
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+      <div class="col-lg-3 col-md-2 col-sm-12 py-5"></div>
+    </div>
+  </div>
+  <div class="container-fluid bg-light py-5">
+    <div class="container">
+      <p class="text-center text-danger h5 mb-0">Questions and Answers</p>
+      <p class="text-center text-dark h2 fw-bold">General FAQs</p>
+      <p class="text-center w-50 mx-auto small mb-0 text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illo veritatis minus voluptatum eos hic natus exercitationem magnam.</p>
+      <hr>
+      <div class="row py-4">
+        <div class="col-lg-6 col-md-6 col-sm-12 px-3">
+          <img src="img/common/photo-1610389372538-90980c897cd3.jpg" class="w-100 rounded-20 shadow" alt="">
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 px-3">
+          <div class="accordion" id="accordionExample">
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q1" aria-expanded="false" aria-controls="collapseOne">
+                  Question 1
+                </button>
+              </h2>
+              <div id="q1" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q2" aria-expanded="false" aria-controls="collapseTwo">
+                  Question 2
+                </button>
+              </h2>
+              <div id="q2" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q3" aria-expanded="false" aria-controls="collapseThree">
+                  Question 3
+                </button>
+              </h2>
+              <div id="q3" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q4" aria-expanded="false" aria-controls="collapseThree">
+                  Question 4
+                </button>
+              </h2>
+              <div id="q4" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#q5" aria-expanded="false" aria-controls="collapseThree">
+                  Question 5
+                </button>
+              </h2>
+              <div id="q5" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="row py-5">
+      <div class="col-lg-2 col-md-1 col-sm-12"></div>
+      <div class="col-lg-8 col-md-10 col-sm-12">
+        <div class="d-flex">
+          <div class="px-4">
+            <i class="fas fa-quote-left  text-secondary display-4"></i>
+          </div>
+          <div class="border w-100 p-3 rounded-3">
+            <p class="h5 text-secondary mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, et ipsum inventore est assumenda velit praesentium dolores cumque perferendis quod consequuntur ducimus dolorem nesciunt quasi eos natus veritatis rem ut!</p>
+            <p class="d-flex align-items-center">
+              <span class="px-3">
+                <img src="img/profile/dribbble.jpg" class="rounded-circle" width="60px" alt="">
+              </span>
+              <span class="fw-bold text-secondary">
+                Name of the Person
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-1 col-sm-12"></div>
+    </div>
+  </div>
+
+
+  <?php include  'inc/footer.php'; ?>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+  <script src="./js/fontawsome.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+
+</html>
